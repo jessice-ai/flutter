4314062987d6663f4040db1d0206151809dfff08
Jessice <jessicesun@gmail.com>
2020-11-09 17:51:46 +0800

    增加返回按钮

--- lib/pages/tabs/sunGoodsList.dart
+++ lib/pages/tabs/sunGoodsList.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/tabs/sunHome.dart';
+import 'sunHome.dart';
 
-class sunGoodsList extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    //throw UnimplementedError();
-    return sunGoodsListSon();
-  }
-
-}
-class sunGoodsListSon extends State{
+class sunGoodsList extends StatelessWidget{
+  String title;
+  int sunCount;
+  sunGoodsList({this.title="商品列表页面",this.sunCount=1});
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     //throw UnimplementedError();
     return Scaffold(
+      //增加返回按钮
+      floatingActionButton: FloatingActionButton(
+        child: Text("返回"),
+        onPressed: (){
+          //路由跳转到某个页面 pop
+          Navigator.of(context).pop(
+              MaterialPageRoute(
+                // ignore: missing_return
+                  builder: (context){
+                    return sunHome();
+                  }
+                //页面控件
+              )
+          );
+        },
+      ),
       appBar: AppBar(
-        title: Text("商品列表页面"),
+        title: Text("${this.title}"),
       ),
       body: ListView(
         children: <Widget>[
           ListTile(
-            title: Text("标题"),
+            title: Text("标题 ${this.sunCount}"),
           ),
           ListTile(
-            title: Text("标题"),
+            title: Text("标题 ${this.sunCount}"),
           ),
           ListTile(
-            title: Text("标题"),
+            title: Text("标题 ${this.sunCount}"),
           ),
         ],
       ),
--- lib/pages/tabs/sunHome.dart
+++ lib/pages/tabs/sunHome.dart
@@ -28,34 +28,34 @@ class sunHomeSon extends State{
                   MaterialPageRoute(
                     // ignore: missing_return
                     builder: (context){
-                      return sunGoodsList();
+                      return sunGoodsList(sunCount: 77,title: "ggg",);
                     }
-                      //页面控件
+                    //页面控件
                   )
               );
           },
           color: Theme.of(context).accentColor, //颜色主题
           textTheme: ButtonTextTheme.primary, //文本主题
         ),
-        SizedBox(height: 20.0,),
-        RaisedButton(
-          child: Text("跳转到宝贝页面"),
-          onPressed: (){
-            //路由跳转到某个页面
-            Navigator.of(context).push(
-                MaterialPageRoute(
-                  // ignore: missing_return
-                    builder: (context){
-                      return sunGoodsList();
-                    }
-                  //页面控件
-                )
-            );
-          },
-          color: Theme.of(context).accentColor, //颜色主题
-          textTheme: ButtonTextTheme.primary, //文本主题
-
-        )
+        // SizedBox(height: 20.0,),
+        // RaisedButton(
+        //   child: Text("跳转到宝贝页面"),
+        //   onPressed: (){
+        //     //基本路由跳转到某个页面
+        //     Navigator.of(context).push(
+        //         MaterialPageRoute(
+        //           // ignore: missing_return
+        //             builder: (context){
+        //               return sunGoodsList(sunCount: 55,titlet: "aaa",);
+        //             }
+        //           //页面控件
+        //         )
+        //     );
+        //   },
+        //   color: Theme.of(context).accentColor, //颜色主题
+        //   textTheme: ButtonTextTheme.primary, //文本主题
+        //
+        // )
       ],
     );
   }
